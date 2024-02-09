a1923da01056a91760d99ad305259e7acc053162
Aliaksandr Karpenka <kasasha@mail.ru>
2024-02-09 11:47:34 +0300

    HW04

---
+++ HW04/src/gb/karpenka/Main.java
@@ -0,0 +1,11 @@
+package gb.karpenka;
+
+import ClientApplication.Start;
+import Services.CashRepository;
+
+public class Main {
+    public static void main(String[] args) {
+        Start start = new Start();
+        start.runLoginRegisterMenu();;
+    }
+}
